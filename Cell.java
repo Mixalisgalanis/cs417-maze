cf1c8c9e87788a9ee5ad150a610838ec571a2342
Mgalanis <Mgalanis@users.noreply.github.com>
2020-04-17 20:40:17 +0300

    Cleaned up main class and added menu system

--- Cell.java
+++ Cell.java
@@ -1,56 +1,75 @@
 /**
-			INTELLIGENCE LAB
-	course		: 	COMP 417 - Artificial Intelligence
-	authors		:	A. Vogiatzis, N. Trigkas
-	excercise	:	1st Programming
-	term 		: 	Spring 2019-2020
-	date 		:   March 2020
-*/
+ * INTELLIGENCE LAB course : COMP 417 - Artificial Intelligence authors : A.
+ * Vogiatzis, N. Trigkas excercise : 1st Programming term : Spring 2019-2020
+ * date : March 2020
+ */
 class Cell {
-	private int cost=1;
+    private int cost = 1;
 
-	private boolean starting_point;
-	private boolean terminal_point;
-	private char cell_type = 'L'; // l stands for Land
+    private boolean starting_point;
+    private boolean terminal_point;
+    private char cell_type = 'L'; // l stands for Land
 
-	Cell (){		
-		this.starting_point = false;
-		this.terminal_point = false;
-	}
+    Cell() {
+        this.starting_point = false;
+        this.terminal_point = false;
+    }
 
-	Cell(char cell_type, boolean starting_point, boolean terminal_point, int world_cost){
-		if(cell_type!= 'L' && cell_type!= 'W' && cell_type!= 'G'){
-			System.out.println("Unknown type of cell. This cell is set to Land!");
-			cell_type='L';
-			world_cost = 1;
-		}
-		this.cell_type = cell_type;
-		this.starting_point = starting_point;
-		this.terminal_point = terminal_point;
-		this.cost = world_cost;
-	}
+    Cell(char cell_type, boolean starting_point, boolean terminal_point, int world_cost) {
+        if (cell_type != 'L' && cell_type != 'W' && cell_type != 'G') {
+            System.out.println("Unknown type of cell. This cell is set to Land!");
+            cell_type = 'L';
+            world_cost = 1;
+        }
+        this.cell_type = cell_type;
+        this.starting_point = starting_point;
+        this.terminal_point = terminal_point;
+        this.cost = world_cost;
+    }
 
-	public boolean isWall(){return  (this.cell_type=='W');}
-	public boolean isGrass(){return  (this.cell_type=='G');}
-	public boolean isLand(){return (this.cell_type=='L');}
+    public boolean isWall() {
+        return (this.cell_type == 'W');
+    }
 
-	public boolean isStart(){return this.starting_point;}
-	public boolean isTerminal(){return this.terminal_point;}
+    public boolean isGrass() {
+        return (this.cell_type == 'G');
+    }
 
-	public int getCost(){return this.cost;}
+    public boolean isLand() {
+        return (this.cell_type == 'L');
+    }
 
-	public void changeCellType(char cell_type, int world_cost){
-		if(cell_type!= 'L' && cell_type!= 'W' && cell_type!= 'G'){
-			System.out.println("Unknown type of cell. This cell is set to Land!");
-			cell_type='L';
-			world_cost = 1;
-		}
-		this.cell_type = cell_type;
-		this.cost = world_cost;
-	}
+    public boolean isStart() {
+        return this.starting_point;
+    }
 
-	public char getCellType(){return this.cell_type;}
-	
-	public void setStartingPoint(boolean sp){this.starting_point=sp;}
-	public void setTerminalPoint(boolean sp){this.terminal_point=sp;}
+    public boolean isTerminal() {
+        return this.terminal_point;
+    }
+
+    public int getCost() {
+        return this.cost;
+    }
+
+    public void changeCellType(char cell_type, int world_cost) {
+        if (cell_type != 'L' && cell_type != 'W' && cell_type != 'G') {
+            System.out.println("Unknown type of cell. This cell is set to Land!");
+            cell_type = 'L';
+            world_cost = 1;
+        }
+        this.cell_type = cell_type;
+        this.cost = world_cost;
+    }
+
+    public char getCellType() {
+        return this.cell_type;
+    }
+
+    public void setStartingPoint(boolean sp) {
+        this.starting_point = sp;
+    }
+
+    public void setTerminalPoint(boolean sp) {
+        this.terminal_point = sp;
+    }
 }
--- GridGenerator.java
+++ GridGenerator.java
@@ -1,3 +1,4 @@
+
 /**
 			INTELLIGENCE LAB
 	course		: 	COMP 417 - Artificial Intelligence
@@ -6,50 +7,93 @@
 	term 		: 	Spring 2019-2020
 	date 		:   March 2020
 */
-import java.util.Random;
 import javax.swing.*;
-import java.awt.Graphics;
-import java.awt.Color;
-import java.util.ArrayList;
 import java.awt.Canvas;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.HashMap;
 
-class GridGenerator{
-	public static void VisualizeGrid(String frame_name, int N, int M, int [] walls, int [] grass, int start_idx, int terminal_idx ){
-		JFrame frame = new JFrame(frame_name);
-		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		Canvas canvas = new Drawing(N,M,walls,grass,start_idx,terminal_idx);
-		canvas.setSize(M*30,N*30);
-		frame.add(canvas);
-		frame.pack();
-		frame.setVisible(true);
-	}
-	public static void VisualizeGrid(String frame_name, int N, int M, int [] walls, int [] grass, int [] steps ,int start_idx, int terminal_idx ){
-		JFrame frame = new JFrame(frame_name);
-		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		Canvas canvas = new Drawing(N,M,walls,grass, steps, start_idx,terminal_idx);
-		canvas.setSize(M*30,N*30);
-		frame.add(canvas);
-		frame.pack();
-		frame.setVisible(true);
-	}
+class GridGenerator {
+    private static final BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
 
-	public static void main(String[] args) {
-		String frame = "Random World";
-		Grid mygrid;
-		if (args.length<1)
-			mygrid = new Grid();
-		else if (args[0].equals("-i")){
-			mygrid = new Grid(args[1]);
-			frame = args[1].split("/")[1];
-		}else if (args[0].equals("-d")){
-			mygrid = new Grid(Integer.parseInt(args[1]),Integer.parseInt(args[2]));
-		}else{
-			mygrid = new Grid("world_examples/default.world");
-			frame = "default.world";
-		}
-		int N = mygrid.getNumOfRows();
-		int M = mygrid.getNumOfColumns();
-		VisualizeGrid(frame,N,M,mygrid.getWalls(),mygrid.getGrass(),mygrid.getStartidx(),mygrid.getTerminalidx());
-	}
-		
+    public static void VisualizeGrid(String frame_name, int N, int M, int[] walls, int[] grass, int start_idx, int terminal_idx) {
+        JFrame frame = new JFrame(frame_name);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        Canvas canvas = new Drawing(N, M, walls, grass, start_idx, terminal_idx);
+        canvas.setSize(M * 30, N * 30);
+        frame.add(canvas);
+        frame.pack();
+        frame.setVisible(true);
+    }
+
+    public static void VisualizeGrid(String frame_name, int N, int M, int[] walls, int[] grass, int[] steps, int start_idx, int terminal_idx) {
+        JFrame frame = new JFrame(frame_name);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        Canvas canvas = new Drawing(N, M, walls, grass, steps, start_idx, terminal_idx);
+        canvas.setSize(M * 30, N * 30);
+        frame.add(canvas);
+        frame.pack();
+        frame.setVisible(true);
+    }
+
+    public static void main(String[] args) {
+        //Configuring World Stats
+        String frame = getFrameName(args);
+        Grid grid = configureWorld(args);
+
+        //Menu System
+        int action = -1;
+        while (action != 5) {
+            displayMainMenu();
+            action = readInt("Action: ");
+            switch (action) {
+            case 0: return;
+            case 1:  break;
+            case 2:  break;
+            case 3:  break;
+            case 4:  break;
+            case 5:  
+                VisualizeGrid(frame, grid.getNumOfRows(), grid.getNumOfColumns(), grid.getWalls(), grid.getGrass(), grid.getStartidx(), grid.getTerminalidx());
+                break;
+            default: System.out.println("Action not found!"); break;
+            }
+        }
+    }
+    
+    private static Grid configureWorld(String[] args) {
+        if (args.length < 1) return new Grid();
+        if (args[0].equals("-i")) return new Grid(args[1]);
+        if (args[0].equals("-d")) return new Grid(Integer.parseInt(args[1]), Integer.parseInt(args[2]));
+        return new Grid("world_examples/default.world");
+    }
+
+    private static String getFrameName(String[] args) {
+        if (args.length < 1 || args[0].equals("-d")) return "Random World";
+        if (args[0].equals("-i")) return args[1].split("/")[1];
+        return "default.world";
+    }
+
+    private static void displayMainMenu(){
+        System.out.println("\n==== Main Menu Options ====");
+        System.out.println("0. Exit");
+        System.out.println("---------------------------");
+		System.out.println("1. BFS");
+		System.out.println("2. DFS");
+		System.out.println("3. A*");
+        System.out.println("4. LRTA*");
+        System.out.println("---------------------------");
+        System.out.println("5. Visualize Original World");
+		System.out.println("===========================");
+    }
+
+    private static int readInt(String message) {
+        System.out.print(message);
+        try {
+            String str = in.readLine();
+            return Integer.parseInt(str);
+        } catch (IOException | NumberFormatException ex) {
+            return -1;
+        }
+    }
 }
